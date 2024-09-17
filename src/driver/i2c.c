087e86887f1e461d29c8f91dc7f10568413d720a
VinbeWan <wanyunbai@outlook.com>
2024-09-17 15:07:14 +0800

    优化IIC驱动，添加链表

--- src/driver/i2c.c
+++ src/driver/i2c.c
@@ -4,6 +4,46 @@
 
 #include "../include/i2c.h"
 
+STATIC_INLINE void IIC_SDA_OUT(void) {
+    io_out32(
+            GPIO_Port, (io_in32(GPIO_Port) | (0x01 << IIC_Pin))
+    );
+}
+STATIC_INLINE void IIC_SDA_OUT_HIGH(void) {
+    io_out32(
+            GPIO_Port, (io_in32(GPIO_Port) | (0x01 << IIC_Pin))
+    );
+}
+STATIC_INLINE void IIC_SDA_OUT_LOW(void) {
+    io_out32(
+            GPIO_Port, (io_in32(GPIO_Port) | (0x00 << IIC_Pin))
+    );
+}
+STATIC_INLINE uint32_t IIC_SDA_IN(void) {
+    return io_in32(GPIO_Port);
+}
+STATIC_INLINE void IIC_SCL_OUT_HIGH(void ) {
+    io_out32(
+            GPIO_Port, (io_in32(GPIO_Port) | (0x01 << IIC_Pin))
+    );
+}
+STATIC_INLINE void IIC_SCL_OUT_LOW(void) {
+    io_out32(
+            GPIO_Port, (io_in32(GPIO_Port) | (0x00 << IIC_Pin))
+    );
+}
+STATIC_INLINE void IIC_Delay(void)
+{
+    volatile uint8_t i;
+    /*
+		*AT32F425F6P7,
+		*i = 100,SCL = 163.4KHZ,6.1us
+		*i = 75, SCL = 243.9KHZ,4.1us
+		*i = 50, SCL = 312.5kHZ,3.2us
+    */
+    for(i=0;i<75;i++);
+}
+
 void IIC_GenerateStart(void){
     IIC_SDA_OUT();
     IIC_SDA_OUT_HIGH();
@@ -101,18 +141,18 @@ int32_t IIC_ReceiveByte(void){
     return temp;
 };
 
-bool IIC_SendWriteAddress(iic_client *client){
+bool IIC_SendWriteAddress(iic_slave_node *client){
     IIC_GenerateStart();
     IIC_SendByte(client->address & 0x01);
     return IIC_ReceiveAcknowledge();
 }
-bool IIC_SendReadAddress(iic_client *client){
+bool IIC_SendReadAddress(iic_slave_node *client){
     IIC_GenerateStart();
     IIC_SendByte(client->address | 0x00);
     return IIC_ReceiveAcknowledge();
 }
 
-bool IIC_CheckClient(iic_client *client){
+bool IIC_CheckClient(iic_slave_node *client){
     IIC_GenerateStart();
     IIC_SendByte(client->address | 0x01);
     IIC_GenerateStop();
--- src/include/i2c.h
+++ src/include/i2c.h
@@ -12,65 +12,26 @@
 #define GPIO_Port 0x12345678    // TODO: change to actual port
 #define IIC_Pin 0x01    // TODO: change to actual pin
 
-STATIC_INLINE void IIC_SDA_OUT(void) {
-    io_out32(
-            GPIO_Port, (io_in32(GPIO_Port) | (0x01 << IIC_Pin))
-    );
-}
-STATIC_INLINE void IIC_SDA_OUT_HIGH(void) {
-    io_out32(
-            GPIO_Port, (io_in32(GPIO_Port) | (0x01 << IIC_Pin))
-    );
-}
-STATIC_INLINE void IIC_SDA_OUT_LOW(void) {
-    io_out32(
-            GPIO_Port, (io_in32(GPIO_Port) | (0x00 << IIC_Pin))
-    );
-}
-STATIC_INLINE uint32_t IIC_SDA_IN(void) {
-    return io_in32(GPIO_Port);
-}
-STATIC_INLINE void IIC_SCL_OUT_HIGH(void ) {
-    io_out32(
-            GPIO_Port, (io_in32(GPIO_Port) | (0x01 << IIC_Pin))
-    );
-}
-STATIC_INLINE void IIC_SCL_OUT_LOW(void) {
-    io_out32(
-            GPIO_Port, (io_in32(GPIO_Port) | (0x00 << IIC_Pin))
-    );
-}
-STATIC_INLINE void IIC_Delay(void)
-{
-    volatile uint8_t i;
-    /*
-		*AT32F425F6P7,
-		*i = 100,SCL = 163.4KHZ,6.1us
-		*i = 75, SCL = 243.9KHZ,4.1us
-		*i = 50, SCL = 312.5kHZ,3.2us
-    */
-    for(i=0;i<75;i++);
-}
-
 typedef struct {
     char name[I2C_NAME_SIZE];
 } iic_device_id;
-typedef struct {
-    uint8_t address;
-    iic_device_id i2CDeviceId;
-} iic_client;
 typedef struct {
     uint16_t length;
     uint8_t  *buffer;
 } iic_message;
 
 typedef struct {
-    int (*probe)(iic_client *, const iic_device_id *);
-    int (*remove)(iic_client *);
+    uint8_t address;
+    iic_device_id iiCDeviceId;
+    List iic_slave_list;
+} iic_slave_node;
+typedef struct {
+    int (*probe)(iic_slave_node *, const iic_device_id *);
+    int (*remove)(iic_slave_node *);
 } iic_driver;
 
-void IIC_client_set_info(iic_client *old_client, iic_client new_client);
-void IIC_client_get_info(iic_client *client);
+void IIC_client_set_info(iic_slave_node *old_client, iic_slave_node new_client);
+void IIC_client_get_info(iic_slave_node *client);
 
 void IIC_GenerateStart(void);
 void IIC_GenerateStop(void);
@@ -82,9 +43,9 @@ bool IIC_ReceiveAcknowledge(void);
 void IIC_SendByte(uint8_t byte);
 int32_t IIC_ReceiveByte(void);
 
-bool IIC_SendWriteAddress(iic_client *);
-bool IIC_SendReadAddress(iic_client *);
+bool IIC_SendWriteAddress(iic_slave_node *);
+bool IIC_SendReadAddress(iic_slave_node *);
 
-bool IIC_CheckClient(iic_client *client);
+bool IIC_CheckClient(iic_slave_node *client);
 
 #endif // CRASHPOWEROS_I2C_H
