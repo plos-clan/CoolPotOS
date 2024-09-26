ff5d1d700f57086540303fc302057c6771b8aebc
xiaoyi1212 <www18845340923@163.com>
2024-09-26 23:01:32 +0800

    更新PCI驱动架构

---
+++ src/driver/ahci.c
@@ -0,0 +1,15 @@
+#include "../include/ahci.h"
+#include "../include/pci.h"
+#include "../include/printf.h"
+#include "../include/vdisk.h"
+
+void ahci_init(){
+    pci_device_t *ahci_ctrl = pci_find_class(0x0106);
+    if(ahci_ctrl == NULL){
+        klogf(false,"Cannot find ahci device\n");
+        return;
+    } else klogf(true,"Find AHCI Controller\n");
+    uint32_t bar5 = pci_dev_read32(ahci_ctrl, PCI_BASE_ADDR5) & 0xFFFFFFF0;
+
+
+}
