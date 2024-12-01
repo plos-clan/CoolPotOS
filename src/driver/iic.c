57bfccd0211218e1e362b9fdf4477096c3af3723
VinbeWan <wanyunbai@outlook.com>
2024-12-01 23:43:39 +0800

    iic驱动重修

---
+++ src/driver/iic.c
@@ -0,0 +1,18 @@
+#include "iic.h"
+
+void crc_check(IIC_Data *frame) {
+    uint32_t *data = frame->data;
+    uint8_t crc = crc8(data, frame->data_len);
+    if (crc != frame->crc) {
+        printk("CRC error!\n");
+        return 0xFFFFFFFF;
+    }else{
+        // 将原始数据转换为32位值
+        uint32_t original_data = 0;
+        for (uint8_t i = 0; i < frame->data_len; i++) {
+            original_data |= (uint32_t)
+            data[i] << (8 * i);
+        }
+        return original_data;
+    }
+};
