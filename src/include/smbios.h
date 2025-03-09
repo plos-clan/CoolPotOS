2700c558ccb079d9e98bf231c824b3cecced3442
xiaoyi1212 <www18845340923@163.com>
2025-03-09 19:30:47 +0800

    更新smbios

--- src/include/smbios.h
+++ src/include/smbios.h
@@ -20,6 +20,23 @@ struct EntryPoint64 {
 	uint64_t StructureTableAddress;	// 结构表地址
 };
 
+struct EntryPoint32 {
+    char AnchorString[4];
+    uint8_t EntryPointChecksum;		// 入口点校验和
+    uint8_t EntryPointLength;		// 入口点结构长度
+    uint8_t SMBIOSMajorVersion;		// SMBIOS主版本
+    uint8_t SMBIOSMinorVersion;		// SMBIOS次版本
+    uint8_t SMBIOSDocrev;			// 文档修订版
+    uint8_t EntryPointRevision;		// 入口点修订版
+    uint8_t FormattedArea[5];       // 保留
+    uint8_t IntermediateAnchorString[5];
+    uint8_t IntermediateChecksum;
+    uint16_t StructureTableMaxSize;	// 结构表最大大小
+    uint32_t StructureTableAddress;	// 结构表地址
+    uint16_t NumberOfStructures;    // 结构数量
+    uint8_t BCDRevision;            // BCD修订
+};
+
 /* 结构表头部 */
 struct Header {
 	uint8_t type;		// 结构类型
