462178501273c8477e5418c44206e185b7192ee7
xiaoyi1212 <www18845340923@163.com>
2025-03-20 18:47:00 +0800

    x86_64合并准备

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,7 @@
 set_project("CoolPotOS")
 
 add_rules("mode.debug", "mode.release")
-add_requires("zig")
+add_requires("zig","nasm")
 
 set_optimize("fastest")
 set_languages("c23")
@@ -10,7 +10,65 @@ set_warnings("all", "extra")
 set_policy("run.autobuild", true)
 set_policy("check.auto_ignore_flags", false)
 
-target("kernel")
+target("kernel32")
+    set_arch("i386")
+    set_kind("binary")
+    set_toolchains("@zig", "nasm")
+    set_toolset("as", "nasm")
+
+    add_cflags("-target x86-freestanding")
+    add_arflags("-target x86-freestanding")
+    add_ldflags("-target x86-freestanding")
+    add_cflags("-mno-mmx", "-mno-sse", "-mno-sse2")
+
+    set_default(false)
+
+    add_linkdirs("libs/i386")
+    add_includedirs("src/i386/include")
+    add_files("src/i386/**/*.asm", "src/i386/**/*.c")
+
+    add_links("os_terminal")
+    add_links("elf_parse")
+    add_links("alloc")
+
+    add_asflags("-f", "elf32")
+    add_ldflags("-T", "src/i386/linker.ld")
+
+target("iso32")
+    set_kind("phony")
+    add_deps("kernel32")
+    set_default(false)
+
+    on_build(function (target)
+        import("core.project.project")
+        local iso_dir = "$(buildir)/iso_dir"
+
+        if os.exists(iso_dir) then os.rmdir(iso_dir) end
+        os.cp("assets", iso_dir)
+
+        local kernel = project.target("kernel32")
+        os.cp(kernel:targetfile(), iso_dir .. "/sys/cpkrnl.elf")
+
+        local iso_file = "$(buildir)/CoolPotOS.iso"
+        local xorriso_flags = "-b sys/limine-bios-cd.bin -no-emul-boot -boot-info-table"
+        os.run("xorriso -as mkisofs %s %s -o %s", xorriso_flags, iso_dir, iso_file)
+        print("ISO image created at: " .. iso_file)
+    end)
+
+--     on_run(function (target)
+--         local misc = "-serial stdio -m 4096"
+--         local speaker = " -audiodev pa,id=speaker -machine pcspk-audiodev=speaker "
+--         local ahci = "-device ahci,id=ahci -drive file=./disk.qcow2,if=none,id=disk0 -device ide-hd,bus=ahci.0,drive=disk0"
+--         local kvm = " -enable-kvm"
+--         local vga = " -vga std -global VGA.vgamem_mb=32 "
+--         local net = " -net nic,model=pcnet -net user "
+--         local audio = " -device sb16,audiodev=speaker -device intel-hda -device hda-micro,audiodev=speaker "
+--         local flags = misc..speaker..vga..net..kvm..audio
+--
+--         os.exec("qemu-system-i386 -cdrom $(buildir)/CoolPotOS.iso %s", flags)
+--     end)
+
+target("kernel64")
     set_kind("binary")
     set_toolchains("clang")
     set_default(false)
@@ -35,10 +93,10 @@ target("kernel")
     add_links("os_terminal")
 
 
-target("iso")
+target("iso64")
     set_kind("phony")
-    add_deps("kernel")
-    set_default(true)
+    add_deps("kernel64")
+    set_default(false)
 
     on_build(function (target)
         import("core.project.project")
@@ -46,7 +104,7 @@ target("iso")
         local iso_dir = "$(buildir)/iso_dir"
         os.cp("assets/*", iso_dir .. "/")
 
-        local target = project.target("kernel")
+        local target = project.target("kernel64")
         os.cp(target:targetfile(), iso_dir .. "/sys/cpkrnl.elf")
 
         local iso_file = "$(buildir)/CoolPotOS.iso"
@@ -59,29 +117,33 @@ target("iso")
         print("ISO image created at: %s", iso_file)
     end)
 
-    on_run(function (target)
-        import("core.project.config")
+--     on_run(function (target)
+--             import("core.project.config")
+--
+--             local flags = {
+--                 "-M", "q35",
+--                 "-cpu", "qemu64,+x2apic",
+--                 "-smp", "4",
+--                 "-serial", "stdio",
+--                 "-m","1024M",
+--                 --"-no-reboot",
+--                 --"-enable-kvm",
+--                 --"-d", "in_asm,int",
+--                 --"-S","-s",
+--                 --"-drive","file=./disk.qcow2,format=raw,id=usbdisk,if=none",
+--                 --"-device","nec-usb-xhci,id=xhci",
+--                 --"-device","usb-storage,bus=xhci.0,drive=usbdisk",
+--                 --"-device","ahci,id=ahci","-drive","file=./disk.qcow2,if=none,id=disk0","-device","ide-hd,bus=ahci.0,drive=disk0",
+--                 --"-drive","file=nvme.raw,if=none,id=D22","-device","nvme,drive=D22,serial=1234",
+--                 --"-audiodev","pa,id=snd","-machine","pcspk-audiodev=snd",
+--                 "-drive", "if=pflash,format=raw,file=assets/ovmf-code.fd",
+--                 "-cdrom", config.buildir() .. "/CoolPotOS.iso",
+--             }
+--
+--             os.execv("qemu-system-x86_64 " , flags)
+--         end)
 
-        local flags = {
-            "-M", "q35",
-            "-cpu", "qemu64,+x2apic",
-            "-smp", "4",
-            "-serial", "stdio",
-            "-m","1024M",
-            --"-no-reboot",
-            --"-enable-kvm",
-            --"-d", "in_asm,int",
-            --"-S","-s",
-            --"-drive","file=./disk.qcow2,format=raw,id=usbdisk,if=none",
-            --"-device","nec-usb-xhci,id=xhci",
-            --"-device","usb-storage,bus=xhci.0,drive=usbdisk",
-            --"-device","ahci,id=ahci","-drive","file=./disk.qcow2,if=none,id=disk0","-device","ide-hd,bus=ahci.0,drive=disk0",
-            --"-drive","file=nvme.raw,if=none,id=D22","-device","nvme,drive=D22,serial=1234",
-            --"-audiodev","pa,id=snd","-machine","pcspk-audiodev=snd",
-            "-drive", "if=pflash,format=raw,file=assets/ovmf-code.fd",
-            "-cdrom", config.buildir() .. "/CoolPotOS.iso",
-            "-hda", "hda.img",
-        }
-        
-        os.execv("qemu-system-x86_64 " , flags)
-    end)
+target("default_build")
+    set_kind("phony")
+    add_deps("iso64")
+    set_default(true)
