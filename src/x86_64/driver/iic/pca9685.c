b4b46a862d2beb3df585528678416d6b58d70687
VinbeWan <wanyunbai@outlook.com>
2025-04-04 19:02:03 +0800

    IIC更新

--- src/x86_64/driver/iic/pca9685.c
+++ src/x86_64/driver/iic/pca9685.c
@@ -61,18 +61,20 @@ uint8_t pca9685_read(uint8_t reg) {
  *
  * @param freq The new frequency for the PCA9685 device, in Hz.
  */
-void pca9685_setPWMFreq(uint32_t freq) {
+void pca9685_setPWMFreq(float freq) {
     // 设置Freq兼容性
-    double prescaleval  = 25000000.0;
-    prescaleval        /= 4096.0;
-    prescaleval        /= (double)freq;
-    prescaleval        -= 1.0;
-    double  prescale    = (prescaleval + 0.5);
-    uint8_t oldmode     = pca9685_read(__MODE1);
-    uint8_t newmode     = (oldmode & 0x7F) | 0x10;
+    freq                *= 0.92;
+    double prescaleval   = 25000000;
+    prescaleval         /= 4096;
+    prescaleval         /= freq;
+    prescaleval         -= 1.0;
+    double  prescale     = prescaleval + 0.5;
+    uint8_t oldmode      = pca9685_read(__MODE1);
+    uint8_t newmode      = (oldmode & 0x7F) | 0x10;
     pca9685_write(__MODE1, newmode);
     pca9685_write(__PRESCALE, (int)(prescale));
     pca9685_write(__MODE1, oldmode);
+    usleep(5000);
     pca9685_write(__MODE1, (oldmode | 0x80));
 }
 
@@ -85,10 +87,11 @@ void pca9685_setPWMFreq(uint32_t freq) {
  */
 void pca9685_setPWM(uint8_t channel, int on, int off) {
     // PWM兼容性
-    pca9685_write(__LED0_ON_L + 4 * channel, on & 0xFF);
-    pca9685_write(__LED0_ON_H + 4 * channel, on >> 8);
+    pca9685_write(__LED0_ON_L  + 4 * channel, on & 0xFF);
+    pca9685_write(__LED0_ON_H  + 4 * channel, on >> 8);
     pca9685_write(__LED0_OFF_L + 4 * channel, off & 0xFF);
     pca9685_write(__LED0_OFF_H + 4 * channel, on >> 8);
+
 }
 
 /**
@@ -99,6 +102,7 @@ void pca9685_setPWM(uint8_t channel, int on, int off) {
  */
 void pca9685_setServoPulse(uint8_t channel, int pulse) {
     // Servo脉冲兼容性
-    pulse = pulse * 4096 / 20000;
+    pulse = pulse * 4096 / 20000;       //PWM frequency is 50HZ,the period is 20000us
     pca9685_setPWM(channel, 0, pulse);
+
 }
