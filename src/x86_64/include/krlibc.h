ace5b31b644c99eddb055f62c785a9948c637eaa
xiaoyi1212 <www18845340923@163.com>
2025-04-06 23:21:07 +0800

    更改内核编号

--- src/x86_64/include/krlibc.h
+++ src/x86_64/include/krlibc.h
@@ -1,14 +1,14 @@
 #pragma once
 
 /**
- * 定义cpinl内核的各种属性
+ * 定义CP_Kernel内核的各种属性
  */
-#define KERNEL_NAME    "cpinl-x86_64-0.1.4" // 内核编号
-#define MAX_CPU        (256)                // 最大支持CPU核心数 256
-#define KERNEL_HEAP    0x800000             // 内核堆大小 8MB
-#define STACK_SIZE     32768                // 栈大小(byte)
-#define KERNEL_ST_SZ   131072               // 增强栈大小 128k
-#define MAX_WAIT_INDEX 100000               // 阻塞最大循环数
+#define KERNEL_NAME    "CP_Kernel-x86_64-0.1.5" // 内核编号
+#define MAX_CPU        (256)                    // 最大支持CPU核心数 256
+#define KERNEL_HEAP    0x800000                 // 内核堆大小 8MB
+#define STACK_SIZE     32768                    // 栈大小(byte)
+#define KERNEL_ST_SZ   131072                   // 增强栈大小 128k
+#define MAX_WAIT_INDEX 100000                   // 阻塞最大循环数
 
 // 常用工具宏
 #define cpu_hlt infinite_loop __asm__("hlt")
