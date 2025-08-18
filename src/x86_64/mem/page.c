004477cd9f4e79f31c77f2983e7697cb81e0f9e9
SegmentationFaultCD <3076253106@qq.com>
2025-08-18 09:28:33 +0800

    Fix some bug

--- src/x86_64/mem/page.c
+++ src/x86_64/mem/page.c
@@ -471,15 +471,24 @@ void page_setup() {
     register_interrupt_handler(14, (void *)page_fault_handle, 0, 0x8E);
     current_directory = &kernel_page_dir;
 }
+
 static void page_map_ranges_help(uint64_t *directory, uint64_t virtual_address,
                      uint64_t physical_address, uint64_t page_count, uint64_t flags, uint64_t offset, uint64_t level) {
-    uint64_t next_index =  virtual_address >> offset;
+    uint64_t next_index =  virtual_address >> 12;
     uint64_t* pmlt_entry = (uint64_t*)(directory[next_index] & 0x000fffffffff000);
+    if (!(directory[next_index] & PTE_PRESENT) ) {
+        if (!pmlt_entry) {
+            pmlt_entry = (uint64_t*)phys_to_virt(alloc_frames(1));
+            directory[next_index] = (uint64_t)virt_to_phys(pmlt_entry) << 12 | PTE_PRESENT | flags;
+        }else {
+            directory[next_index] |= PTE_PRESENT;
+        }
+    }
     if (level) {
         page_map_ranges_help(pmlt_entry, virtual_address, physical_address, page_count, flags, offset - 9, level - 1);
     }
     else {
-        flags |= (offset != 12 ? 1ul << 7 : 0);
+        flags |= (offset != 12 ? PTE_HUGE : 0);
         for (uint64_t i = 0; i < page_count; ++i) {
             directory[next_index] = 0;
             directory[next_index] |= (physical_address << offset) | flags;
