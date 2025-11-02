de34abcab42e4d477320e205dee63462df44cb15
MicroFish <2609948707@qq.com>
2025-11-02 15:03:01 +0800

    修复了一个不起眼的内存泄漏。

--- src/fs/vfs.c
+++ src/fs/vfs.c
@@ -277,7 +277,10 @@ errno_t vfs_mkfile(const char *name) {
         parent = vfs_open(fullpath);
     }
 
-    if (parent == NULL || parent->type != file_dir) { return VFS_STATUS_FAILED; }
+    if (parent == NULL || parent->type != file_dir) {
+        free(fullpath);
+        return VFS_STATUS_FAILED;
+    }
 
     // 创建文件
     vfs_node_t node = vfs_child_append(parent, filename, NULL);
