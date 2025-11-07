a9e0841c3f04590106e80d01c256b85bcb8e077e
xiaoyi1212 <www18845340923@163.com>
2025-11-08 00:29:39 +0800

    增加栈保护检查代码.
    Co-authored-by: suhuajun-github <115517663+suhuajun-github@users.noreply.github.com>

--- src/include/security.h
+++ src/include/security.h
@@ -2,6 +2,17 @@
 
 #include "types.h"
 
-extern uintptr_t __stack_chk_guard;
+struct pthread {
+    struct pthread *self;
+    uintptr_t *dtv;
+    struct pthread *prev, *next; /* non-ABI */
+    uintptr_t       sysinfo;
+    uintptr_t canary;
+};
 
-void init_stack_canary();
+/**
+ * 注意: 不得在调用此函数的上下文进行函数返回.
+ */
+__attr(always_inline) void init_stack_canary(void);
+
+void test_stack_chk(void) ;
