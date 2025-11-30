08c6d8ffb45fa0d1367a3772ba52a2b295b31f7f
xiaoyi1212 <www18845340923@163.com>
2025-11-30 23:41:39 +0800

    进一步修改procfs派发实现, 实现 meminfo. (#80)
    Co-authored-by: Rainy101112 <Rainy101112@163.com>

--- src/fs/procfs/proc_dpatch.c
+++ src/fs/procfs/proc_dpatch.c
@@ -24,14 +24,17 @@ static uint64_t hash_dp(const char *s) {
     return h;
 }
 
-static void create_procfs_node(char *name, read_entry_t read_entry, stat_entry_t stat_entry) {
+static void create_procfs_handle(char *name, read_entry_t read_entry, stat_entry_t stat_entry){
     proc_handle_node_t *handle = malloc(sizeof(proc_handle_node_t));
     handle->name               = strdup(name);
     handle->hash               = hash_dp(handle->name);
     handle->read_entry         = read_entry;
     handle->stat_entry         = stat_entry;
     dispatch_array[dp_index++] = handle;
+}
 
+static void create_procfs_node(char *name, read_entry_t read_entry, stat_entry_t stat_entry) {
+    create_procfs_handle(name,read_entry,stat_entry);
     vfs_node_t node        = vfs_node_alloc(procfs_root, name);
     node->type             = file_none;
     node->mode             = 0700;
@@ -47,6 +50,11 @@ void load_procfs_root() {
     create_procfs_node("interrupts", proc_interrupts_read, proc_interrupts_stat);
     create_procfs_node("filesystems", proc_filesystems_read, proc_filesystems_stat);
     create_procfs_node("kmsg", proc_kmsg_read, proc_kmsg_stat);
+    create_procfs_node("meminfo", proc_meminfo_read, proc_meminfo_stat);
+
+    create_procfs_handle("proc_cmdline",proc_pcmdline_read,proc_pcmdline_stat);
+    create_procfs_handle("proc_maps",proc_pmaps_read,proc_pmaps_stat);
+    create_procfs_handle("proc_stat",proc_pstat_read,proc_pstat_stat);
 }
 
 size_t procfs_read_dispatch(proc_handle_t *handle,void *addr, size_t offset, size_t size){
