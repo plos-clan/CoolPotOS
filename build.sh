d06240c14bba4eeba97ad89799c63a814deecaf7
A4-Tacks <wdsjxhno1001@163.com>
2025-12-17 10:07:02 +0800

    Add fail-fast for build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+set -e
 cmake -S . -B build/ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DTARGET_ARCH=x86_64
 cmake --build build/ --target run -j 12
 cp build/compile_commands.json compile_commands.json
